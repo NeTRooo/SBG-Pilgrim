61fa86e24269bdd9c84a53c6b649ebbe166f52cf
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-02-28 13:39:30 +0300

    start project

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib.auth import logout
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+import json
+import requests
+from allauth.socialaccount.models import SocialAccount
+from django.contrib.auth.models import User, Group
+from rest_framework.views import APIView
 
-# Create your views here.
+import uuid
+import random
+import string
+import hashlib
+import base64
+import time
+
+def main_home(request):
+    return render(request, 'main_page/main.html')
