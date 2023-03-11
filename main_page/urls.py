ef99de81ff6a298095c8a25dbcd62666eaa10062
DenSSS <dimon.yablonovskiyy@mail.ru>
2023-03-12 00:55:11 +0300

    Первая рабочая версия сайта. First working v site. Добавлена страница авторизации, главная страница, профиль пользователя

--- main_page/urls.py
+++ main_page/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_home, name='main_page'),
+    path('', views.main_page, name='main_page'),
+    path('login/', views.login_page, name='login_page'),
+    path('profile/', views.profile_page, name='profile_page'),
+    path('check/', views.check_page, name='check_page'),
+    path('applications/', views.applications_page, name='applications_page'),
+    path('help/', views.help_page, name='help_page'),
+    path('settings/', views.settings_page, name='settings_page'),
+    path('logout/', views.custom_logout, name='custom_logout'),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,21 +1,54 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseRedirect
 from django.contrib.auth import logout
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 import json
 import requests
 from allauth.socialaccount.models import SocialAccount
-from django.contrib.auth.models import User, Group
-from rest_framework.views import APIView
 
-import uuid
-import random
-import string
-import hashlib
-import base64
-import time
 
-def main_home(request):
-    return render(request, 'main_page/main.html')
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('profile_page')
+    else:
+        return render(request, 'main_page/login.html')
+
+def main_page(request):
+    if request.user.is_authenticated:
+        return render(request, 'main_page/main.html')
+    else:
+        return redirect('login_page')
+
+def profile_page(request):
+    if request.user.is_authenticated:
+        return render(request, 'main_page/profile.html')
+    else:
+        return redirect('login_page')
+
+def check_page(request):
+    if request.user.is_authenticated:
+        return render(request, 'main_page/check.html')
+    else:
+        return redirect('login_page')
+
+def applications_page(request):
+    if request.user.is_authenticated:
+        return render(request, 'main_page/applications.html')
+    else:
+        return redirect('login_page')
+
+def help_page(request):
+    if request.user.is_authenticated:
+        return render(request, 'main_page/help.html')
+    else:
+        return redirect('login_page')
+
+def settings_page(request):
+    if request.user.is_authenticated:
+        return render(request, 'main_page/settings.html')
+    else:
+        return redirect('login_page')
+
+def custom_logout(request):
+    logout(request)
+    return redirect('main_page')
+
